2b3b32ef0f56cfe8dad8bb05f7d68aec41002501
Marko Kuburić <markokuburic02@gmail.com>
2024-11-03 23:13:21 +0100

    added-user-service-and-controller-classes

---
+++ src/main/java/rs/ac/uns/ftn/onlybuns/controller/UserController.java
@@ -0,0 +1,50 @@
+package rs.ac.uns.ftn.onlybuns.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.ftn.onlybuns.model.User;
+import rs.ac.uns.ftn.onlybuns.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        // Set createdAt and ensure no ID is manually assigned
+        user.setCreatedAt(LocalDateTime.now());
+        user.setId(null); // Ensure ID is not manually set
+        User createdUser = userService.saveUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.getUserByUsername(username);
+        return user != null ? new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/rs/ac/uns/ftn/onlybuns/service/UserService.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.onlybuns.service;
+
+import rs.ac.uns.ftn.onlybuns.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user); // Method for saving a new user
+    User getUserByUsername(String username); // Method for retrieving a user by username
+    List<User> getAllUsers(); // Method for retrieving all users
+    User updateUser(Long id, User updatedUser); // Method for updating a user
+    void deleteUser(Long id); // Method for deleting a user
+}
