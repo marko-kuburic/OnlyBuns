8d94b31a19c339cb16cdd1592bbb4d43551f646e
Marko Kuburić <markokuburic02@gmail.com>
2024-11-03 23:03:47 +0100

    added-Spring-Boot-Initializr-setup

---
+++ src/main/java/rs/ac/uns/ftn/onlybuns/OnlybunsApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.onlybuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlybunsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlybunsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/uns/ftn/onlybuns/OnlybunsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.onlybuns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlybunsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
