0ed85b0bea1147ab238cb4c26c6fecf7f3b534aa
Marko Kuburić <markokuburic02@gmail.com>
2024-11-04 15:26:26 +0100

    fixed-post-request-in-post-controller-so-it-could-submit-images-binary

--- onlybuns/src/main/java/rs/ac/uns/ftn/onlybuns/controller/PostController.java
+++ onlybuns/src/main/java/rs/ac/uns/ftn/onlybuns/controller/PostController.java
@@ -2,11 +2,14 @@ package rs.ac.uns.ftn.onlybuns.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import rs.ac.uns.ftn.onlybuns.model.Post;
 import rs.ac.uns.ftn.onlybuns.service.PostService;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,10 +25,24 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<Post> createPost(
+            @RequestParam("userId") Long userId,
+            @RequestParam("locationId") Long locationId,
+            @RequestParam("content") String content,
+            @RequestParam("image") MultipartFile image) throws IOException {
+
+        // Handle the image and convert it to byte[] if needed
+        byte[] imageData = image.getBytes();
+
+        Post post = new Post();
+        post.setUserId(userId);
+        post.setLocationId(locationId);
+        post.setContent(content);
+        post.setImageData(imageData);
         post.setCreatedAt(LocalDateTime.now());
         post.setUpdatedAt(LocalDateTime.now());
+
         Post createdPost = postService.createPost(post);
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
