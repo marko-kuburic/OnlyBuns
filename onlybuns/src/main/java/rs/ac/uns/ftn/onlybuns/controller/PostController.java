c92097655f5878c665b0f24477665d80e63e1c73
Marko Kuburić <markokuburic02@gmail.com>
2024-11-04 14:28:14 +0100

    moved-backend-to-separate-folder

--- onlybuns/src/main/java/rs/ac/uns/ftn/onlybuns/controller/PostController.java
+++ onlybuns/src/main/java/rs/ac/uns/ftn/onlybuns/controller/PostController.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/posts")
 public class PostController {
 
